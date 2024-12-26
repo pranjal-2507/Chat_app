ae1f54f6b003b4ec0ef19c6f1a5380c20b5c979f
Pranjal <pranjalgosavi2507@gmail.com>
2024-12-26 14:28:46 +0530

    Arreaged the files properly

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react'; // ChakraProvider stays here
-import { extendTheme } from '@chakra-ui/react'; // Use '@chakra-ui/react' for version 2.x or '@chakra-ui/theme' for 3.x
+import { ChakraProvider } from '@chakra-ui/react'; 
+import { extendTheme } from '@chakra-ui/react'; 
 import App from './App';
 
 // Define the theme
